df39df9b7a94df234d9a7a70dde196edc39ba401
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-02 21:05:06 +0800

    Update s02_01.py

--- s02_01.py
+++ s02_01.py
@@ -1,15 +1,10 @@
 def is_prime(num):
-    #even number
-    if num % 2 == 0:
-        print(f'{num} is not a prime number')
-        return False
-    a = 3
-    for i in range(num//2 - 1):
-        if num % a == 0:
+    
+    for i in range(2, num):
+        if num % i == 0:
             print(f'{num} is not a prime number')
-            # print(f'{num} has a factor {a}')
+            # print(f'{num} has a factor {i}')
             return False
-        a += 2
 
     print(f'{num} is a prime number')
     return True
