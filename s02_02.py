6b2c7b1f64c5f9a571e5b441f2c58d80fd9b1885
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-02 18:03:17 +0800

    Update s02_02.py

--- s02_02.py
+++ s02_02.py
@@ -1,6 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser(description = 'check if the number is prime or not')
+parser = argparse.ArgumentParser(description = 'check if the number is a prime number or not')
 parser.add_argument('n1', type = int, default = 1, help = 'number')
 
 args = parser.parse_args()
@@ -9,17 +9,17 @@ number = args.n1
 def is_prime(num):
     #even number
     if num % 2 == 0:
-        print(f'{num} is not a prime')
+        print(f'{num} is not a prime number')
         return False
     a = 3
     for i in range(num//2 - 1):
         if num % a == 0:
-            print(f'{num} is not a prime')
+            print(f'{num} is not a prime number')
             # print(f'{num} has a factor {a}')
             return False
         a += 2
 
-    print(f'{num} is a prime')
+    print(f'{num} is a prime number')
     return True
 
 is_prime(number)
