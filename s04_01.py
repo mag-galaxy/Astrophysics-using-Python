4627654d70a7332cae9b97d76a0053d02e42ac76
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-16 14:09:14 +0800

    Update s04_01.py

--- s04_01.py
+++ s04_01.py
@@ -7,8 +7,10 @@ import sys
 
 parser = argparse.ArgumentParser(description='plotting f(x)')
 # add argument
-parser.add_argument ('-o', '--output', default='output.png', help='output file name (default: output.png)')
-parser.add_argument ('-r', '--resolution', type=float, default=250.0, help='resolution of plot in DPI (default: 250.0)')
+parser.add_argument ('-o', '--output', default='output.png',\
+                     help='output file name (default: output.png)')
+parser.add_argument ('-r', '--resolution', type=float, default=250.0,\
+                     help='resolution of plot in DPI (default: 250.0)')
 args = parser.parse_args()
 
 file_output = args.output
@@ -22,7 +24,10 @@ if(path_output.exists()):
     print (f'ERROR: output file "{file_output}" exists!')
     sys.exit(0)
 
-if not((path_output.suffix == '.eps')or(path_output.suffix == '.pdf')or(path_output.suffix == '.png')or(path_output.suffix == '.ps')):
+if not((path_output.suffix == '.eps')\
+       or(path_output.suffix == '.pdf')\
+       or(path_output.suffix == '.png')\
+       or(path_output.suffix == '.ps')):
     print (f'ERROR: output file must be either EPS or PDF or PNG or PS file.')
     sys.exit(0)
 
