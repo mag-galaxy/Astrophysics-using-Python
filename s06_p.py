0d48df2e5252a4255ed05e9e69c0f59e426d1e7f
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-26 17:55:52 +0800

    Update s06_p.py

--- s06_p.py
+++ s06_p.py
@@ -1,2 +1,22 @@
-import numpy
 import argparse
+import ssl
+import urllib.request
+parser = argparse.ArgumentParser(description = 'download data')
+parser.add_argument('-url', default = '', help = 'URL of data')
+
+args = parser.parse_args()
+
+link = args.url
+ssl._create_default_https_context = ssl._create_unverified_context
+
+with urllib.request.urlopen(link) as f_read:
+  data_read = f_read.read().decode('utf-8')
+
+total_price = 0
+
+all_data = data_read.split('\n')# by line
+for i in range(8,len(all_data)):
+  item_data = all_data.split() 
+  total_price += int(item_data[1])*int(item_data[2])
+
+print(f'total price: {total_price}')
