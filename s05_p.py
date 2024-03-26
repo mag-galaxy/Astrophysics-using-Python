8ffcf0cd12b6d1eb148b5121a3522823e05b088c
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-26 13:50:05 +0800

    Update s05_p.py

--- s05_p.py
+++ s05_p.py
@@ -11,10 +11,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 with urllib.request.urlopen(link) as f_read:
   data_read = f_read.read().decode('utf-8')
-# print(data_read)
+
 all_data = data_read.split('\n')
 for i in range(len(all_data)):
-   print(all_data[i])
+   print('{i}. : {all_data[i]}')
 print(f'data number: {len(all_data)}')
 
 total = 0
