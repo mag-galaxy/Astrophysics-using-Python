6f97868e3804fc3c62e8182cea2eac3547f2ade7
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-26 13:45:56 +0800

    Update s05_p.py

--- s05_p.py
+++ s05_p.py
@@ -11,15 +11,15 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 with urllib.request.urlopen(link) as f_read:
   data_read = f_read.read().decode('utf-8')
-print(data_read)
+# print(data_read)
 all_data = data_read.split('\n')
 for i in range(len(all_data)):
    print(all_data[i])
-print(f'data number: {len(data_read)}')
+print(f'data number: {len(all_data)}')
 
 total = 0
-for i in range(len(data_read)):
-  total += float(data_read[i])
+for i in range(len(all_data)):
+  total += float(all_data[i])
 print(f'total: {total}')
-mean = total/len(data_read)
+mean = total/len(all_data)
 print(f'mean = {mean}')
