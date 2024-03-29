b12e77b939e3debedc5b5c19b9f3e4e531c1da6c
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-29 19:23:49 +0800

    Update s06_01_02.py

--- s06_01_02.py
+++ s06_01_02.py
@@ -1,1 +1,37 @@
 # download readme file
+
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://cdsarc.cds.unistra.fr/ftp/I/131A/ReadMe'
+
+# output file name
+file_output = 'sao.readme'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# converting raw byte data into string
+data_str = data_byte.decode ('utf-8')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'w') as fh_write:
+    # writing data
+    fh_write.write (data_str)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
