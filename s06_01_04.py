1e2e1430d9eb8cbaf341cc5142c051e92924de3d
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-01 19:44:20 +0800

    Update s06_01_04.py

--- s06_01_04.py
+++ s06_01_04.py
@@ -1,5 +1,4 @@
 # add data to SQL
-
 import gzip
 import sys
 
@@ -10,8 +9,8 @@ file_sql = 'sao.sql'
 with open(file_sql, 'w') as fh_sql:
   # SQL command to create table
   sql_table = f'create table sao (sao integer primary key,' \
-        + f'vmag real, ' \
-        + f'sptype text);\n'
+        + f'ra text, dec text, ' \
+        + f'vmag real, sptype text);\n'
   fh_sql.write(sql_table)
 
   #open catalogue file
@@ -24,6 +23,30 @@ with open(file_sql, 'w') as fh_sql:
         print("ERROR: cannot extract SAO number!")
         sys.exit(1)
 
+      # Right Ascension
+      try:
+        RAh = int(line[7:9])
+        RAm = int(line[9:11])
+        RAs = float(line[11:17])
+      except:
+        RAh = 99
+        RAm = 99
+        RAs = 99.9
+      RA = f'{RAh:02d}:{RAm:02d}:{RAs:04.1f}'
+  
+      # Declination
+      try:
+        Dec_sign = line[41:42].decode('utf-8')
+        Dec_d = int(line[42:44])
+        Dec_m = int(line[44:46])
+        Dec_s = int(line[46:51])
+      except:
+        Dec_sign = '-'
+        Dec_d = 99
+        Dec_m = 99
+        Dec_s = 99
+      Dec = f'{Dec_sign}{Dec_d:02d}:{Dec_m:02d}:{Dec_s:02d}'
+      
       # Visual Magnitude
       try:
         Vmag = float(line[80:84])# Vmag Byte 81~84
@@ -35,6 +58,6 @@ with open(file_sql, 'w') as fh_sql:
 
       # SQL command to add data to table
       sql_add = f'insert into sao values ({SAO}, ' \
-          + f'{Vmag}, ' \
-          + f'"{SpType}");\n'
+          + f'"RA", "Dec", ' \
+          + f'{Vmag}, "{SpType}");\n'
       fh_sql.write(sql_add)
