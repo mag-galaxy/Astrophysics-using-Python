587dbe9008e5b993db4f1edc5dd378ab6fd12e21
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-01 20:00:39 +0800

    Update s06_01_04.py

--- s06_01_04.py
+++ s06_01_04.py
@@ -22,31 +22,13 @@ with open(file_sql, 'w') as fh_sql:
       except:
         print("ERROR: cannot extract SAO number!")
         sys.exit(1)
-
-      # Right Ascension
-      try:
-        RAh = int(line[150:152])
-        RAm = int(line[152:154])
-        RAs = float(line[154:160])
-      except:
-        RAh = 99
-        RAm = 99
-        RAs = 99.9
-      RA = f'{RAh:02d}:{RAm:02d}:{RAs:04.1f}'
-  
-      # Declination
-      try:
-        Dec_sign = line[167:168].decode('utf-8')
-        Dec_d = int(line[168:170])
-        Dec_m = int(line[170:172])
-        Dec_s = int(line[172:177])
-      except:
-        Dec_sign = '-'
-        Dec_d = 99
-        Dec_m = 99
-        Dec_s = 99
-      Dec = f'{Dec_sign}{Dec_d:02d}:{Dec_m:02d}:{Dec_s:02d}'
       
+      # Photographic Magnitude
+      try:
+        Pmag = float(line[76:80])# Vmag Byte 81~84
+      except:
+        Pmag = -999 # an impossible value
+        
       # Visual Magnitude
       try:
         Vmag = float(line[80:84])# Vmag Byte 81~84
@@ -58,6 +40,5 @@ with open(file_sql, 'w') as fh_sql:
 
       # SQL command to add data to table
       sql_add = f'insert into sao values ({SAO}, ' \
-          + f'"RA", "Dec", ' \
-          + f'{Vmag}, "{SpType}");\n'
+          + f'{Pmag}, {Vmag}, "{SpType}");\n'
       fh_sql.write(sql_add)
