78e2ca49714864ee32417ddc88331a6bd99f692c
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-01 20:01:34 +0800

    Update s06_01_03.py

--- s06_01_03.py
+++ s06_01_03.py
@@ -14,29 +14,11 @@ with gzip.open(file_catalogue, 'rb') as fh:
       print("ERROR: cannot extract SAO number!")
       sys.exit(1)
 
-    # Right Ascension
-    try:
-      RAh = int(line[150:152])
-      RAm = int(line[152:154])
-      RAs = float(line[154:160])
-    except:
-      RAh = 99
-      RAm = 99
-      RAs = 99.9
-    RA = f'{RAh:02d}:{RAm:02d}:{RAs:04.1f}'
-
-    # Declination
-    try:
-      Dec_sign = line[167:168].decode('utf-8')
-      Dec_d = int(line[168:170])
-      Dec_m = int(line[170:172])
-      Dec_s = int(line[172:177])
-    except:
-      Dec_sign = '-'
-      Dec_d = 99
-      Dec_m = 99
-      Dec_s = 99
-    Dec = f'{Dec_sign}{Dec_d:02d}:{Dec_m:02d}:{Dec_s:02d}'
+    # Photographic Magnitude
+      try:
+        Pmag = float(line[76:80])# Vmag Byte 81~84
+      except:
+        Pmag = -999 # an impossible value
     
     # Visual Magnitude
     try:
@@ -49,7 +31,6 @@ with gzip.open(file_catalogue, 'rb') as fh:
 
     # print extracted data
     print(f'SAO = {SAO}')
-    print(f'Right Ascension = {RA}')
-    print(f'Declination = {Dec}')
+    print(f'Pmag = {Pmag}')
     print(f'Vmag = {Vmag}')
     print(f'SpType = {SpType}')
