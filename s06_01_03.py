e8c28361ad68d0870280771ab219f5d7cba96a61
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-01 19:39:58 +0800

    Update s06_01_03.py

--- s06_01_03.py
+++ s06_01_03.py
@@ -1,5 +1,4 @@
 # read data from catalogue
-
 import gzip
 import sys
 
@@ -15,6 +14,30 @@ with gzip.open(file_catalogue, 'rb') as fh:
       print("ERROR: cannot extract SAO number!")
       sys.exit(1)
 
+    # Right Ascension
+    try:
+      RAh = int(line[7:9])
+      RAm = int(line[9:11])
+      RAs = float(line[11:17])
+    except:
+      RAh = 99
+      RAm = 99
+      RAs = 99.9
+    RA = f'{RAh:02d}:{RAm:02d}:{RAs:04.1f}'
+
+    # Declination
+    try:
+      Dec_sign = line[41:42].decode('utf-8')
+      Dec_d = int(line[42:44])
+      Dec_m = int(line[44:46])
+      Dec_s = int(line[46:51])
+    except:
+      Dec_sign = '-'
+      Dec_d = 99
+      Dec_m = 99
+      Dec_s = 99
+    Dec = f'{Dec_sign}{Dec_d:02d}:{Dec_m:02d}:{Dec_s:02d}'
+    
     # Visual Magnitude
     try:
       Vmag = float(line[80:84])# Vmag Byte 81~84
@@ -26,5 +49,7 @@ with gzip.open(file_catalogue, 'rb') as fh:
 
     # print extracted data
     print(f'SAO = {SAO}')
+    print(f'Right Ascension = {RA}')
+    print(f'Declination = {Dec}')
     print(f'Vmag = {Vmag}')
     print(f'SpType = {SpType}')
