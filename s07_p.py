c1d5a66906b59612cb24952dd0c830fd9fabf4ad
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-02 18:10:00 +0800

    Update s07_p.py

--- s07_p.py
+++ s07_p.py
@@ -4,7 +4,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 link = 'https://s3b.astro.ncu.edu.tw/appy_202402/data/p07.data'    # URL of data
-file_sql = 'sao.sql'
+file_sql = 'star.sql'
 
 with urllib.request.urlopen(link) as f_read:
   data_read = f_read.read().decode('utf-8')
@@ -13,7 +13,7 @@ all_data = data_read.split('\n')# by line
 
 with open(file_sql, 'w') as fh_sql:
   # SQL command to create table
-  sql_table = f'create table sao (name text primary key,' \
+  sql_table = f'create table star (name text primary key,' \
         + f'app_mag real, abs_mag real, sptype text);\n'
   fh_sql.write(sql_table)
 
