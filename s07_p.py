a234ec11ecc02a5357e17ed56a0369db9e5f312c
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-02 17:58:45 +0800

    Update s07_p.py

--- s07_p.py
+++ s07_p.py
@@ -1,1 +1,10 @@
+import urllib.request
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
+
+link = 'https://s3b.astro.ncu.edu.tw/appy_202402/data/p07.data'    # URL of data
+file_output = 'sao.dat.gz'    # output file name
+
+with urllib.request.urlopen(link) as f_read:
+  data_read = f_read.read().decode('utf-8')
