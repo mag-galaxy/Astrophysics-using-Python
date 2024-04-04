94aa60a614ca174a0fcfae1a3a18c44153199fdc
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-04 23:57:51 +0800

    Update s07_01.py

--- s07_01.py
+++ s07_01.py
@@ -1,4 +1,3 @@
-# importing astropy module
 import astropy.coordinates
 import astropy.time
 import astropy.units
@@ -6,12 +5,8 @@ import astropy.units
 # using "DE440" for solar system ephemeris
 astropy.coordinates.solar_system_ephemeris.set ('de440')
 
-# units
-u_m = astropy.units.m
-
-# date/time in UTC
-t_str = '2024-04-03 04:00:00'
-t_utc = astropy.time.Time (t_str, format='iso', scale='utc')
+m = astropy.units.m
+UTC = astropy.time.Time ('2024-04-24 13:00:00', format='iso', scale='utc')
 
 # location of observer: NCU main campus
 longitude = '121d11m12s'
@@ -19,13 +14,16 @@ latitude  = '+24d58m12s'
 height    = 151.6 * u_m
 observer  = astropy.coordinates.EarthLocation (longitude, latitude, height)
 
-# getting position of the Sun
-sun = astropy.coordinates.get_body ('sun', t_utc, location=observer)
+# position of the Sun
+sun = astropy.coordinates.get_body ('sun', UTC, location = observer)
 
-# printing position of the Sun
-print (f'position of the Sun as observed at NCU main campus at {t_utc}:')
-print (f'  RA:  {int (sun.ra.hms.h):02d}:{int (sun.ra.hms.m):02d}', \
-       f':{sun.ra.hms.s:06.3f}', sep='')
-print (f'  Dec: {int (sun.dec.dms.d):02d}', \
-       f':{abs (int (sun.dec.dms.m)):02d}:{abs (sun.dec.dms.s):06.3f}', \
-       sep='')
+# change ra to azimuth, dec to elevation
+print(f'position of the Sun as observed at NCU main campus at {UTC}:')
+print(f' azimuth: {}')
+print(f' elevation: {sun.dec.dms.d + sun.dec.dms.m / 60 + sun.dec.dms.s / 3600}')
+
+# print (f'  RA:  {int (sun.ra.hms.h):02d}:{int (sun.ra.hms.m):02d}', \
+#        f':{sun.ra.hms.s:06.3f}', sep='')
+# print (f'  Dec: {int (sun.dec.dms.d):02d}', \
+#        f':{abs (int (sun.dec.dms.m)):02d}:{abs (sun.dec.dms.s):06.3f}', \
+#        sep='')
