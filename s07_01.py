63647d72f5ac0890c930ac3945959c8ac4291173
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-06 12:08:26 +0800

    Update s07_01.py

--- s07_01.py
+++ s07_01.py
@@ -1,4 +1,5 @@
 # Moon position at 2024-04-24 13:00:00 (utc)
+# Moon position at 2024-04-24 21:00:00 (in Taiwan)
 import astropy.time
 import astropy.units
 import astropy.coordinates
@@ -7,6 +8,7 @@ import astropy.coordinates
 astropy.coordinates.solar_system_ephemeris.set ('de440')
 
 m = astropy.units.m
+t_Taiwan = astropy.time.Time ('2024-04-24 21:00:00', format='iso', scale='utc')
 UTC = astropy.time.Time ('2024-04-24 13:00:00', format='iso', scale='utc')
 
 # location of observer: Mt. Jade
@@ -27,5 +29,5 @@ moon_altaz = moon.transform_to(altaz)
 moon_az = moon_altaz.az
 moon_alt = moon_altaz.alt
 
-print(f'position of the Moon as observed at Mt. Jade at {UTC}:')
+print(f'position of the Moon as observed at Mt. Jade at {t_Taiwan}:')
 print(f'(azimuth, elevation) = ({moon_az}, {moon_alt})')
