308a10f67b85c6c715f26138b57456b4fc080d10
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-08 18:58:24 +0800

    Update s07_02.py

--- s07_02.py
+++ s07_02.py
@@ -7,7 +7,7 @@ import astroquery.skyview
 import matplotlib.figure
 import matplotlib.backends.backend_agg
 
-# files
+# files names
 download_file = 'm101.fits'
 output_file = 'm101.png'
 resolution = 250
@@ -15,10 +15,10 @@ resolution = 250
 object_name = 'M101'
 survey = 'DSS2 Red'
 
-# field-of-view
+# field-of-view  = 45 * 45
 fov_arcmin = 45.0
 fov_arcsec = fov_arcmin * 60.0
-npixel     = int (fov_arcsec)
+npixel = int(fov_arcsec)
 
 # colour map
 cmap = 'magma'
@@ -27,57 +27,51 @@ cmap = 'magma'
 ha  = astropy.units.hourangle
 deg = astropy.units.deg
 
-# name resolver
-query_result = astroquery.simbad.Simbad.query_object (object_name)
+# get query result
+query_result = astroquery.simbad.Simbad.query_object(object_name)
 
 # coordinate from Simbad
-ra_str  = query_result['RA'][0]
+ra_str = query_result['RA'][0]
 dec_str = query_result['DEC'][0]
 
 # making SkyCoord object of astropy
-coord = astropy.coordinates.SkyCoord (ra_str, dec_str, frame='icrs', \
+coord = astropy.coordinates.SkyCoord(ra_str, dec_str, frame='icrs', \
                                       unit=(ha, deg) )
-(RA, Dec) = coord.to_string (style='hmsdms').split ()
+(RA, Dec) = coord.to_string(style='hmsdms').split ()
 
 # printing result
 print (f'Target name: "{object_name}"')
-print (f'  RA  = {RA}')
+print (f'  RA = {RA}')
 print (f'  Dec = {Dec}')
 
 # getting a list of images
-list_image = astroquery.skyview.SkyView.get_image_list (position=coord, \
+list_image = astroquery.skyview.SkyView.get_image_list(position=coord, \
                                                         survey=survey)
 
 # printing a list of images found
 print ("images =", list_image)
 
 # getting images
-images = astroquery.skyview.SkyView.get_images (position=coord, \
+images = astroquery.skyview.SkyView.get_images(position=coord, \
                                                 survey=survey, \
                                                 pixels=npixel)
 
 # image
-image  = images[0]
+image = images[0]
 header = image[0].header
-data   = image[0].data
+data = image[0].data
 
-# printing image information
-print (image.info ())
+print(image.info ())
 
-print (f'Writing a FITS file "{file_fits}"...')
-
+print(f'Writing a FITS file "{file_fits}"...')
 # writing FITS file
 hdu = astropy.io.fits.PrimaryHDU (data = data, header = header)
 hdu.writeto (file_fits)
-
 print (f'Done!')
 
 # opening FITS file
 with astropy.io.fits.open (file_fits) as hdu_list:
-    # printing HDU information
     print (hdu_list.info ())
-    
-    # reading FITS header, WCS information, and image data
     header = hdu_list[0].header
     wcs    = astropy.wcs.WCS (header)
     image  = hdu_list[0].data
