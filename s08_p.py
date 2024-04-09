8609cd5fb25f6a420beff3d1014263aeca58c24a
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-09 18:26:16 +0800

    Update s08_p.py

--- s08_p.py
+++ s08_p.py
@@ -4,8 +4,12 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 link = 'https://www.minorplanetcenter.net/iau/MPCORB/NEA.txt'    # URL of data
-
 file_sql = 'asteriod.sql'
+
+with urllib.request.urlopen(link) as f_read:
+  data_read = f_read.read().decode('utf-8')
+
+all_data = data_read.split('\n')# by line
 
 with open(file_sql, 'w') as fh_sql:
   # SQL command to create table
@@ -13,11 +17,9 @@ with open(file_sql, 'w') as fh_sql:
         + f'semi real);\n'
   fh_sql.write(sql_table)
 
-  with urllib.request.urlopen(link) as f_read:
-    # data_read = f_read.read().decode('utf-8')
-    for line in f_read:
-      number = line[0:7].strip().decode('utf-8')
-      semi = float(line[92:103])
+  for i in range(len(all_data))
+      number = all_data[i][0:7].strip().decode('utf-8')
+      semi = float(all_data[i][92:103])
     
     sql_add = f'insert into asteriod values ("{number}", ' \
           + f'{semi});\n'
