46b641c8bd05ceafcceb64a35c1848bd7f197103
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-09 18:51:52 +0800

    Update s08_p.py

--- s08_p.py
+++ s08_p.py
@@ -4,22 +4,25 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 link = 'https://www.minorplanetcenter.net/iau/MPCORB/NEA.txt'    # URL of data
+file_txt = 'asteriod.txt'
 file_sql = 'asteriod.sql'
 
-with urllib.request.urlopen(link) as f_read:
-  data_read = f_read.read().decode('utf-8')
+with urllib.request.urlopen (link) as fh_read:
+    data_byte = fh_read.read ()
 
-all_data = data_read.split('\n')# by line
+with open (file_txt, 'wb') as fh_write:
+    fh_write.write (data_byte)
 
-with open(file_sql, 'w') as fh_sql:
-  # SQL command to create table
-  sql_table = f'create table asteriod (number text primary key,' \
-        + f'semi real);\n'
+print('finish writing data to .txt')
+
+with open (file_sql, 'w') as fh_sql:
+  sql_table = f'create table asteriod (asteriod text primary key, semi real);\n'
   fh_sql.write(sql_table)
-
-  for i in range(len(all_data)):
-      number = all_data[i][0:7].strip().decode('utf-8')
-      semi = float(all_data[i][92:103])
-    
-    sql_add = f'insert into asteriod values ("{number}", {semi});\n'
-    fh_sql.write(sql_add)
+  
+  with open(file_txt, 'rb') as fh:
+    for line in fh:
+      number = line[0:7].strip().decode('utf-8')
+      semi = float(line[92:103])
+      
+      sql_add = f'insert into asteriod values ("{number}", {semi});\n'
+      fh_sql.write(sql_add)
