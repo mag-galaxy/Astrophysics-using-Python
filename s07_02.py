74feb395ce2241c8ebb2d584e73fb400f9d02550
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-09 08:52:43 +0800

    Update s07_02.py

--- s07_02.py
+++ s07_02.py
@@ -24,66 +24,62 @@ npixel = int(fov_arcsec)
 cmap = 'magma'
 
 # units
-ha  = astropy.units.hourangle
+ha = astropy.units.hourangle
 deg = astropy.units.deg
 
 # get query result
-query_result = astroquery.simbad.Simbad.query_object(object_name)
+query_m101 = astroquery.simbad.Simbad.query_object(object_name)
 
 # coordinate from Simbad
-ra_str = query_result['RA'][0]
-dec_str = query_result['DEC'][0]
+ra_str = query_m101['RA'][0]
+dec_str = query_m101['DEC'][0]
 
 # making SkyCoord object of astropy
-coord = astropy.coordinates.SkyCoord(ra_str, dec_str, frame='icrs', \
-                                      unit=(ha, deg) )
-(RA, Dec) = coord.to_string(style='hmsdms').split ()
+coord = astropy.coordinates.SkyCoord(ra_str, dec_str, frame = 'icrs', \
+                                      unit = (ha, deg))
+(RA, Dec) = coord.to_string(style = 'hmsdms').split()
 
-# printing result
-print (f'Target name: "{object_name}"')
+print (f'Target name: {object_name}')
 print (f'  RA = {RA}')
 print (f'  Dec = {Dec}')
 
 # getting a list of images
-list_image = astroquery.skyview.SkyView.get_image_list(position=coord, \
-                                                        survey=survey)
-
-# printing a list of images found
+list_image = astroquery.skyview.SkyView.get_image_list(position = coord, \
+                                                        survey = survey)
 print ("images =", list_image)
 
 # getting images
-images = astroquery.skyview.SkyView.get_images(position=coord, \
-                                                survey=survey, \
-                                                pixels=npixel)
+images = astroquery.skyview.SkyView.get_images(position = coord, \
+                                                survey = survey, \
+                                                pixels = npixel)
 
 # image
 image = images[0]
 header = image[0].header
 data = image[0].data
+print(image.info())
 
-print(image.info ())
-
-print(f'Writing a FITS file "{file_fits}"...')
 # writing FITS file
-hdu = astropy.io.fits.PrimaryHDU (data = data, header = header)
-hdu.writeto (file_fits)
+print(f'Writing a FITS file "{download_file}"...')
+hdu = astropy.io.fits.PrimaryHDU(data = data, header = header)
+hdu.writeto (download_file)
 print (f'Done!')
 
 # opening FITS file
-with astropy.io.fits.open (file_fits) as hdu_list:
-    print (hdu_list.info ())
+with astropy.io.fits.open(download_file) as hdu_list:
+    print(hdu_list.info())
     header = hdu_list[0].header
-    wcs    = astropy.wcs.WCS (header)
-    image  = hdu_list[0].data
+    wcs = astropy.wcs.WCS(header)
+    image = hdu_list[0].data
 
-# objects figure, canvas, axes
-fig = matplotlib.figure.Figure ()
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
-ax = fig.add_subplot (111, projection=wcs)
+# objects figure, canvas, axes for plotting
+fig = matplotlib.figure.Figure()
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
+ax = fig.add_subplot(111, projection = wcs)
 
-ax.set_title (object_name)
-ax.set_xlabel ('Right Ascension')
-ax.set_ylabel ('Declination')
+ax.set_title(object_name)
+ax.set_xlabel('Right Ascension')
+ax.set_ylabel('Declination')
 
 # normalisation
 norm \
@@ -91,8 +87,8 @@ norm \
     ( stretch=astropy.visualization.AsinhStretch () )
 
 # plot
-im = ax.imshow(image, origin='lower', cmap = cmap, norm = norm)
+im = ax.imshow(image, origin = 'lower', cmap = cmap, norm = norm)
 fig.colorbar(im)
 
-print(f'{file_fits} ==> {file_png}')
-fig.savefig(file_png, dpi=resolution_dpi)
+print(f'{download_file} ==> {output_file}')
+fig.savefig(output_file, dpi = resolution)
