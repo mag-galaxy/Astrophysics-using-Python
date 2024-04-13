870c8cb3cfd1c49a731ae120a901a55ae5148265
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-13 09:54:42 +0800

    Update s08_a_01.py

--- s08_a_01.py
+++ s08_a_01.py
@@ -1,8 +1,8 @@
+# download data
 import urllib.request
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
-
 link = 'https://iopscience.iop.org/0004-637X/865/1/77/suppdata/apjaad81bt3_ascii.txt?doi=10.3847/1538-4357/aad81b'
 
 # output file name
@@ -10,19 +10,19 @@ file_output = 'hd98800_b.data'
 
 print (f'Fetching {link}...')
 
-# opening URL
+# open URL and read
 with urllib.request.urlopen (link) as fh_read:
-    data_byte = fh_read.read ()
+    data_byte = fh_read.read()
 
 print (f'Fetched {link}!')
 
 # converting raw byte data into string
-data_str = data_byte.decode ('utf-8')
+data_str = data_byte.decode('utf-8')
 
 print (f'Now, writing data into file "{file_output}"...')
 
-# opening file for writing
+# write data
 with open (file_output, 'w') as fh_write:
-    fh_write.write (data_str)
+    fh_write.write(data_str)
 
 print (f'Finished writing data into file "{file_output}"!')
