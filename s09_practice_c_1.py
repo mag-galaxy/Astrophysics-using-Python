221c101b903c6c0b59d18e6ee3b12c3a3a7b20f1
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-16 20:17:06 +0800

    Update and rename s09_practice_c.py to s09_practice_c_1.py

---
+++ s09_practice_c_1.py
@@ -0,0 +1,21 @@
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url_data = 'https://cdsarc.cds.unistra.fr/viz-bin/nph-Cat/txt.gz?V/84/cstar.dat.gz'    # URL of data
+file_output = 'cstar.dat.gz'    # output file name
+
+print (f'Now, fetching {url_data}...')
+# open URL and read data
+with urllib.request.urlopen (url_data) as fh_read:
+    data_byte = fh_read.read ()
+
+print (f'Finished fetching {url_data}!')
+print (f'Now, writing data into file "{file_output}"...')
+
+# write data to output file
+with open (file_output, 'wb') as fh_write:
+    fh_write.write (data_byte)
+
+print (f'Finished writing data into file "{file_output}"!')
