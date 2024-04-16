e1c5351d8ae00b53b168d6fcdaf500c0cdf349b4
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-16 18:03:30 +0800

    Update s09_p.py

--- s09_p.py
+++ s09_p.py
@@ -5,6 +5,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 link = 'https://cdsarc.cds.unistra.fr/ftp/cats/I/239/hip_main.dat'    # URL of data
 file_output = 'hip.data'    # output file name
+file_sql = 'hip.sql'
 
 print (f'Now, fetching {link}...')
 # open URL and read data
@@ -20,19 +21,30 @@ with open (file_output, 'wb') as fh_write:
 
 print (f'Finished writing data into file "{file_output}"!')
 
-with open(file_output, 'rb') as fh:
-  for line in fh:
-    try:
-      HIP = int(line[8:14])
-    except:
-      print("ERROR: cannot extract HIP number!")
-      sys.exit(1)
+# create table
+with open(file_sql, 'w') as fh_sql:
+    # SQL command to create table
+    sql_table = f'create table sao (hip integer primary key,' \
+            + f' B_V real);\n'
+    fh_sql.write(sql_table)
+    with open(file_output, 'rb') as fh:
+      for line in fh:
+        try:
+          HIP = int(line[8:14])
+        except:
+          print("ERROR: cannot extract HIP number!")
+          sys.exit(1)
+    
+        # Right Ascension
+        try:
+            B_V = float(line[244:251])
+        except:
+    
+        # print extracted data
+        print(f'HIP = {HIP}')
+        print(f'  B_V = {B_V}')
 
-    # Right Ascension
-    try:
-        B_V = float(line[244:251])
-    except:
-
-    # print extracted data
-    print(f'HIP = {HIP}')
-    print(f'  B_V = {B_V}')
+        # SQL command to add data to table
+        sql_add = f'insert into sao values ({SAO}, ' \
+          + f', {Vmag});\n'
+        fh_sql.write(sql_add)
