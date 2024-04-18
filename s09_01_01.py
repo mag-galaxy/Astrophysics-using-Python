ff44711320d00446ce49688c1b9fbf7a886b9355
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-18 21:31:58 +0800

    Update and rename s09_01.py to s09_01_01.py

---
+++ s09_01_01.py
@@ -0,0 +1,15 @@
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+link = 'http://astro.phys.wvu.edu/wise/wise_hii_V2.2.csv'
+file_output = 'hii.data'    # output file name
+
+print (f'Now, fetching {link}...')
+with urllib.request.urlopen (link) as fh_read:
+    data_byte = fh_read.read ()
+    print(data_byte)
+
+with open (file_output, 'wb') as fh_write:
+    fh_write.write (data_byte)
+print (f'Finished writing data into file "{file_output}"!')
