beeb8b7de75ee8c504db2320254e1b397c061d54
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-21 19:37:00 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -16,5 +16,3 @@ with urllib.request.urlopen (link) as fh_read:
             data_str = line.decode('utf-8')
             data_list = data_str.split(',')
             print(data_list)
-
-#print (f'Finished writing data into file "{file_output}"!')
