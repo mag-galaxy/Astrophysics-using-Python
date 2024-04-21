3692bdd2fd1f22c92073eab83eb7bc7d10b4d75c
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-21 19:38:27 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -3,9 +3,8 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 link = 'http://astro.phys.wvu.edu/wise/wise_hii_V2.2.csv'
-file_output = 'hii.data'    # output file name
+file_output = 'glactic_hii.data'    # output file name
 
-print (f'Now, fetching {link}...')
 with urllib.request.urlopen (link) as fh_read:
     i = 0
     for line in fh_read:
