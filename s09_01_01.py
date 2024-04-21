f346abb34f274fac862bd9fe28cb3c994124e9bc
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-21 12:01:16 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -9,9 +9,7 @@ print (f'Now, fetching {link}...')
 with urllib.request.urlopen (link) as fh_read:
     for line in fh_read:
         data_str = line.decode('utf-8')
-        print(data_str)
-        print()
         data_list = data_str.split(',')
         print(data_list)
 
-print (f'Finished writing data into file "{file_output}"!')
+#print (f'Finished writing data into file "{file_output}"!')
