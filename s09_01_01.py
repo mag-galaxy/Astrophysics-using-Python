0ae505bad97ed075a87799e2fcbda1a8fa3fa7e8
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-21 21:20:02 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -8,12 +8,14 @@ file_output = 'glactic_hii.data'    # output file name
 
 # write long. and lat. data into output file
 with urllib.request.urlopen (link) as fh_read:
-    i = 0
-    for line in fh_read:
-        if i==0:
-            ++i
-            continue
-        else:
-            data_str = line.decode('utf-8')
-            data_list = data_str.split(',')
-            print(data_list)
+    data_read = fh_read.read().decode('utf-8')
+    print(data_read)
+    # i = 0
+    # for line in fh_read:
+    #     if i==0:
+    #         ++i
+    #         continue
+    #     else:
+    #         data_str = line.decode('utf-8')
+    #         data_list = data_str.split(',')
+    #         print(data_list)
