d2fda47acd5d9d673c5e52dfb18a7c0c2a26230d
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-22 20:12:05 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -24,19 +24,18 @@ with urllib.request.urlopen (link) as fh_read:
         if i==0:                # skip first line
             ++i
             continue
-        else:
-            data_str = line.decode('utf-8')
-            data_list = data_str.split(',')
-            gal_l = float(data_list[2])    # glactic longitude
-            gal_b = float(data_list[3])    # glactic latitude
-            print(gal_l + ' ' + gal_b)
-            gal_coord = astropy.coordinates.Galactic(l=gal_l, b=gal_b)
-            ra_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).ra.wrap_at(180.0*u_deg).radian
-            dec_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).dec.radian    
-            # if(ra_deg > 180):
-            #     ra_deg -= 360.0
-            list_ra_deg.append(ra_deg)
-            list_dec_deg.append(dec_deg)
+        data_str = line.decode('utf-8')
+        data_list = data_str.split(',')
+        gal_l = float(data_list[2])    # glactic longitude
+        gal_b = float(data_list[3])    # glactic latitude
+        print(gal_l + ' ' + gal_b)
+        gal_coord = astropy.coordinates.Galactic(l=gal_l, b=gal_b)
+        ra_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).ra.wrap_at(180.0*u_deg).radian
+        dec_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).dec.radian    
+        # if(ra_deg > 180):
+        #     ra_deg -= 360.0
+        list_ra_deg.append(ra_deg)
+        list_dec_deg.append(dec_deg)
             
 # change into numpy arrays
 array_ra_deg = numpy.array(list_ra_deg)
