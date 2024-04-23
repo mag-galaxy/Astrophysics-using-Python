90beff03f02f5f6e0fab2ac9f5f5f5944ddf85d1
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 12:56:03 +0800

    Update s09_01_02.py

--- s09_01_02.py
+++ s09_01_02.py
@@ -16,8 +16,22 @@ u_deg = astropy.units.degree
 list_ra_deg = []
 list_dec_deg = []
 
+# add data to list
 with open(data_input, 'r') as f_read:
-  #
+  for line in f_read:
+    data = line.split(',')
+    gal_l = float(data[0])    # glactic longitude
+    gal_b = float(data[1])    # glactic latitude
+    print(gal_l + ' ' + gal_b)
+    gal_coord = astropy.coordinates.Galactic(l = gal_l, b = gal_b)
+    ra_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).ra.wrap_at(180.0*u_deg).radian
+    dec_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).dec.radian    
+    list_ra_deg.append(ra_deg)
+    list_dec_deg.append(dec_deg)
+
+# change into numpy arrays
+array_ra_deg = numpy.array(list_ra_deg)
+array_dec_deg = numpy.array(list_dec_deg)
 
 # ecliptic plane
 ecl_lon = numpy.linspace(0.001, 359.999, 1000) * u_deg
