faa5e561705672395538e29a31be6bf51ef9a5ea
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 16:13:26 +0800

    Update s09_01_02.py

--- s09_01_02.py
+++ s09_01_02.py
@@ -19,26 +19,26 @@ u_deg = astropy.units.degree
 list_ra_deg = []
 list_dec_deg = []
 
-# add data to list
-with open(data_input, 'r') as f_read:
-  for line in f_read:
-    data = line.split(',')
-    gal_l = float(data[0]) * u_deg   # glactic longitude
-    gal_b = float(data[1]) * u_deg   # glactic latitude
+# # add data to list
+# with open(data_input, 'r') as f_read:
+#   for line in f_read:
+#     data = line.split(',')
+#     gal_l = float(data[0]) * u_deg   # glactic longitude
+#     gal_b = float(data[1]) * u_deg   # glactic latitude
 
-    galactic_coords = astropy.coordinates.SkyCoord(l=gal_l, b=gal_b, frame='galactic')
-    equatorial_coords = galactic_coords.transform_to('icrs')
-    ra_deg = equatorial_coords.ra
-    dec_deg = equatorial_coords.dec
-    list_ra_deg.append(ra_deg)
-    list_dec_deg.append(dec_deg)
+#     galactic_coords = astropy.coordinates.SkyCoord(l=gal_l, b=gal_b, frame='galactic')
+#     equatorial_coords = galactic_coords.transform_to('icrs')
+#     ra_deg = equatorial_coords.ra
+#     dec_deg = equatorial_coords.dec
+#     list_ra_deg.append(ra_deg)
+#     list_dec_deg.append(dec_deg)
 
-# change into numpy arrays
-array_ra_deg = numpy.array(list_ra_deg)
-array_dec_deg = numpy.array(list_dec_deg)
+# # change into numpy arrays
+# array_ra_deg = numpy.array(list_ra_deg)
+# array_dec_deg = numpy.array(list_dec_deg)
 
-print(array_ra_deg)
-print(array_dec_deg)
+# print(array_ra_deg)
+# print(array_dec_deg)
 
 # ecliptic plane
 ecl_lon = numpy.linspace(0.001, 359.999, 1000) * u_deg
@@ -62,8 +62,8 @@ ax.grid()
 ax.set_xlabel('Right Ascension [deg]')
 ax.set_ylabel('Declination [deg]')
 ax.set_title(f'Distribution of Galictic Hii regions on {date}', loc='right')
-ax.plot(numpy.deg2rad(array_ra_deg), numpy.deg2rad(array_dec_deg), linestyle='None',
-         marker='o', markersize=3, color='blue', alpha=0.1, label='Glactic Hii')
+# ax.plot(numpy.deg2rad(array_ra_deg), numpy.deg2rad(array_dec_deg), linestyle='None',
+#          marker='o', markersize=3, color='blue', alpha=0.1, label='Glactic Hii')
 # ax.scatter(array_ra_deg, array_dec_deg, marker='o', s=1, c=blue, \
 #           cmap=matplotlib.cm.Spectral, alpha=0.25)
 ax.plot (ecl_ra, ecl_dec, linestyle='None', marker='o', markersize=5, \
