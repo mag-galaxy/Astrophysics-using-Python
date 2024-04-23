8054a11bc0487358345d1cde1af5f23b7f4112bd
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 20:44:27 +0800

    Update s10_01_01.py

--- s10_01_01.py
+++ s10_01_01.py
@@ -1,1 +1,9 @@
+import urllib.request
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
+link = 'http://astro.phys.wvu.edu/wise/wise_hii_V2.2.csv'
+data_output = 'data.txt'
+
+# download data
+with urllib.request.urlopen (link) as fh_read:
