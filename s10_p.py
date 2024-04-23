53064de8f33b85e506f670c1ed53bc414446ba73
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 18:01:06 +0800

    Update s10_p.py

--- s10_p.py
+++ s10_p.py
@@ -12,7 +12,7 @@ with urllib.request.urlopen (link) as fh_read:
     fh_sql.write(sql_table)
     
     data_byte = fh_read.read().decode('utf-8')
-    Name = line[0:11].strip()
+    Name = data_byte[0:11].strip()
     Vmag = float(data_byte[38:44])
     
     sql_add = f'insert into UBVRI values ("{Name}", {Vmag});\n'
