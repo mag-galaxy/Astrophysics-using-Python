c4efc4f72c56511894fbe92ef4177ba0937450a0
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 12:41:12 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -38,36 +38,3 @@ array_ra_deg = numpy.array(list_ra_deg)
 array_dec_deg = numpy.array(list_dec_deg)
 
 print('finish adding data')
-# date = astropy.time.Time('2024-04-22 00:00:00')
-
-# # ecliptic plane
-# ecl_lon = numpy.linspace(0.001, 359.999, 1000) * u_deg
-# ecl_lat = numpy.zeros(1000) * u_deg
-# ecl_coord = astropy.coordinates.GeocentricMeanEcliptic(lon=ecl_lon, lat=ecl_lat, obstime=date)
-# ecl_ra = ecl_coord.transform_to(astropy.coordinates.ICRS()).ra.wrap_at(180.0*u_deg).radian
-# ecl_dec = ecl_coord.transform_to(astropy.coordinates.ICRS()).dec.radian
-
-# # galactic plane
-# gal_lon = numpy.linspace(0.001, 359.999, 1000) * u_deg
-# gal_lat = numpy.zeros(1000) * u_deg
-# gal_coord = astropy.coordinates.Galactic(l = gal_lon, b = gal_lat)
-# gal_ra = gal_coord.transform_to(astropy.coordinates.ICRS()).ra.wrap_at(180.0*u_deg).radian
-# gal_dec = gal_coord.transform_to(astropy.coordinates.ICRS()).dec.radian
-
-# # figure, canvas, axes object for plotting
-# fig = matplotlib.figure.Figure()
-# canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
-# ax = fig.add_subplot (111, projection='mollweide')        # use Mollweide projection
-# ax.grid()
-# ax.set_xlabel('Right Ascension [deg]')
-# ax.set_ylabel('Declination [deg]')
-# ax.set_title(f'Distribution of Galictic Hii regions on {date}', loc='right')
-# ax.plot(numpy.deg2rad(array_ra_deg), numpy.deg2rad(array_dec_deg), linestyle='None',
-#          marker='o', markersize=1, color='blue', alpha=0.1, label='Glactic Hii')
-# ax.plot (ecl_ra, ecl_dec, linestyle='None', marker='o', markersize=5, \
-#          color='yellow', alpha=0.5, label='Ecliptic plane')
-# ax.plot (gal_ra, gal_dec, linestyle='None', marker='o', markersize=5, \
-#          color='silver', alpha=0.5, label='Galactic plane')
-# ax.legend (bbox_to_anchor = (0.9, -0.1))
-
-# fig.savefig(png_output, dpi=resolution_dpi)
