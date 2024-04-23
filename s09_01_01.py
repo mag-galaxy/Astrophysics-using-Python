1346eb92713e06d71c3d6909f22ded52c22bf33e
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 12:50:34 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -10,13 +10,9 @@ link = 'http://astro.phys.wvu.edu/wise/wise_hii_V2.2.csv'
 data_output = 'data.txt'
 
 # download data
-i = 0
 with urllib.request.urlopen (link) as fh_read:
     with open(data_output, 'w') as f_write:
         for line in fh_read:
-            if i==0:
-                ++i
-                continue
             data_str = line.decode('utf-8')
             data_list = data_str.split(',')
             f_write.write(data_list[2] + ',' + data_list[3] + '\n')
