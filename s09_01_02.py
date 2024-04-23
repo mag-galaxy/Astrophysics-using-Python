801316605720f29c59f34d58e57501c0d0d52208
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 13:42:52 +0800

    Update s09_01_02.py

--- s09_01_02.py
+++ s09_01_02.py
@@ -10,6 +10,9 @@ date = astropy.time.Time('2024-04-22 00:00:00')
 data_input = 'data.txt'
 png_output = 'glactic_hii.png'     # plot and save as .png file
 resolution_dpi = 150
+
+# units
+u_ha  = astropy.units.hourangle
 u_deg = astropy.units.degree
 
 # empty lists for storing data
@@ -20,18 +23,22 @@ list_dec_deg = []
 with open(data_input, 'r') as f_read:
   for line in f_read:
     data = line.split(',')
-    print(data[0] + ' ' + data[1])
     gal_l = float(data[0]) * u_deg   # glactic longitude
     gal_b = float(data[1]) * u_deg   # glactic latitude
-    gal_coord = astropy.coordinates.Galactic(l = gal_l, b = gal_b)
-    ra_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).ra.wrap_at(180.0*u_deg).radian
-    dec_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).dec.radian    
+
+    galactic_coords = astropy.coordinates.SkyCoord(l=gal_l, b=gal_b, frame='galactic')
+    equatorial_coords = galactic_coords.transform_to('icrs')
+    ra_deg = equatorial_coords.ra
+    dec_deg = equatorial_coords.dec
     list_ra_deg.append(ra_deg)
     list_dec_deg.append(dec_deg)
 
 # change into numpy arrays
 array_ra_deg = numpy.array(list_ra_deg)
 array_dec_deg = numpy.array(list_dec_deg)
+
+print(array_ra_deg)
+print(array_dec_deg)
 
 # ecliptic plane
 ecl_lon = numpy.linspace(0.001, 359.999, 1000) * u_deg
@@ -55,12 +62,13 @@ ax.grid()
 ax.set_xlabel('Right Ascension [deg]')
 ax.set_ylabel('Declination [deg]')
 ax.set_title(f'Distribution of Galictic Hii regions on {date}', loc='right')
-ax.plot(numpy.deg2rad(array_ra_deg), numpy.deg2rad(array_dec_deg), linestyle='None',
-         marker='o', markersize=1, color='blue', alpha=0.1, label='Glactic Hii')
+# ax.plot(numpy.deg2rad(array_ra_deg), numpy.deg2rad(array_dec_deg), linestyle='None',
+#          marker='o', markersize=3, color='blue', alpha=0.1, label='Glactic Hii')
+ax.scatter(array_ra_deg, array_dec_deg, marker='o', s=1, c=blue, \
+          cmap=matplotlib.cm.Spectral, alpha=0.25)
 ax.plot (ecl_ra, ecl_dec, linestyle='None', marker='o', markersize=5, \
          color='yellow', alpha=0.5, label='Ecliptic plane')
 ax.plot (gal_ra, gal_dec, linestyle='None', marker='o', markersize=5, \
          color='silver', alpha=0.5, label='Galactic plane')
-ax.legend (bbox_to_anchor = (0.9, -0.1))
 
 fig.savefig(png_output, dpi=resolution_dpi)
