9768a0f8cdb07ac54fd583d68fd3ad01ad411dae
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-23 12:55:29 +0800

    Update s09_01_01.py

--- s09_01_01.py
+++ s09_01_01.py
@@ -1,9 +1,5 @@
 import urllib.request
 import ssl
-import numpy
-import astropy.coordinates
-import astropy.time
-import astropy.units
 
 ssl._create_default_https_context = ssl._create_unverified_context
 link = 'http://astro.phys.wvu.edu/wise/wise_hii_V2.2.csv'
@@ -19,19 +15,4 @@ with urllib.request.urlopen (link) as fh_read:
                 continue
             f_write.write(data_list[2] + ',' + data_list[3] + '\n')
 f_write.close()
-#             gal_l = float(data_list[2])    # glactic longitude
-#             gal_b = float(data_list[3])    # glactic latitude
-#             print(gal_l + ' ' + gal_b)
-#             gal_coord = astropy.coordinates.Galactic(l=gal_l, b=gal_b)
-#             ra_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).ra.wrap_at(180.0*u_deg).radian
-#             dec_deg = gal_coord.transform_to(astropy.coordinates.ICRS ()).dec.radian    
-#             # if(ra_deg > 180):
-#             #     ra_deg -= 360.0
-#             list_ra_deg.append(ra_deg)
-#             list_dec_deg.append(dec_deg)
-            
-# # change into numpy arrays
-# array_ra_deg = numpy.array(list_ra_deg)
-# array_dec_deg = numpy.array(list_dec_deg)
-
-print('finish adding data')
+print('finish downloading data')
