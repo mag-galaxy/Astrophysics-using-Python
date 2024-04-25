3dfb4a64d95042afbf25d07a7cbd61d80bb96d99
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-25 20:09:23 +0800

    Update s10_01_01.py

--- s10_01_01.py
+++ s10_01_01.py
@@ -1,1 +1,8 @@
+import astropy.io.ascii
 
+# CSV file name
+file_csv = 'Leda_data_1.csv'
+
+# reading a CSV file and storing data in an astropy table
+table = astropy.io.ascii.read(file_csv, format='csv')
+print(table)
