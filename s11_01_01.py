ce78b49ce87998b50e54a8c8d4f4810b4f838705
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-05 15:04:50 +0800

    Update s11_01_01.py

--- s11_01_01.py
+++ s11_01_01.py
@@ -1,5 +1,4 @@
-# .gz file -> position of stars graph -> find distance of moat stars -> proper motion -> list file
-# download data from Gaia Catalogue
+import argparse
 import astropy.units
 import astropy.coordinates
 import astroquery.simbad
@@ -8,39 +7,42 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# settings for Gaia query
+# Gaia query
 astroquery.gaia.Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 astroquery.gaia.Gaia.ROW_LIMIT = -1
 
-# parameters
-target        = "M25"
-file_output   = "m25.vot.gz"
-radius_arcmin = 30.0
-radius_deg    = radius_arcmin / 60.0
+# using argparse
+descr = 'Gaia DR3 Catalogue downloading'
+parser = argparse.ArgumentParser(description=descr)
+parser.add_argument('-t', '--target', help='target name')
+parser.add_argument('-o', '--output', help='output file name')
+parser.add_argument('-r', '--radius', type=float, help='radius of search in arcmin')
+args = parser.parse_args ()
+
+# get value from argument
+target = args.target
+file_output = args.output
+radius_arcmin = args.radius
+radius_deg = radius_arcmin / 60.0
 
 # units
-u_deg    = astropy.units.deg
-u_ha     = astropy.units.hourangle
+u_deg = astropy.units.deg
+u_ha = astropy.units.hourangle
 u_arcmin = astropy.units.arcmin
 
-# name resolver
-result_simbad = astroquery.simbad.Simbad.query_object (target)
-
-# coordinate from Simbad
-obj_ra  = result_simbad['RA'][0]
-obj_dec = result_simbad['DEC'][0]
-
-# using skycoord of astropy
-coord = astropy.coordinates.SkyCoord (obj_ra, obj_dec, frame='icrs', \
-                                      unit=(u_ha, u_deg) )
+# find target using Simbad and get its coordinate using Skycoord
+result_simbad = astroquery.simbad.Simbad.query_object(target)
+tar_ra = result_simbad['RA'][0]
+tar_dec = result_simbad['DEC'][0]
+coord = astropy.coordinates.SkyCoord(tar_ra, tar_dec, frame='icrs', unit=(u_ha, u_deg) )
 
 # coordinate in decimal degree
-ra_deg  = coord.ra.deg
+ra_deg = coord.ra.deg
 dec_deg = coord.dec.deg
 
 # coordinate in hms and dms format
-ra_hms  = coord.ra.to_string (u_ha)
-dec_dms = coord.dec.to_string (u_deg, alwayssign=True)
+ra_hms = coord.ra.to_string(u_ha)    # hr:min:sec
+dec_dms = coord.dec.to_string(u_deg, alwayssign=True)    # deg:arcmin:arcsec
 
 # printing result
 print (f"target: {target}")
@@ -48,15 +50,13 @@ print (f" RA  = {ra_hms:>14s} = {ra_deg:10.6f} deg")
 print (f" Dec = {dec_dms:>14s} = {dec_deg:+10.6f} deg")
 
 # command for database query
-table  = f"gaiadr3.gaia_source"
-field  = f"*"
-point  = f"POINT({ra_deg:8.4f},{dec_deg:8.4f})"
+table = f"gaiadr3.gaia_source"
+field = f"*"
+point = f"POINT({ra_deg:8.4f},{dec_deg:8.4f})"
 circle = f"CIRCLE(ra,dec,{radius_deg})"
-query  = f"SELECT {field} from {table} WHERE 1=CONTAINS({point},{circle});"
-
-# printing SQL query for Gaia database
-print (f"SQL query for Gaia database:")
-print (f" {query}")
+query = f"SELECT {field} from {table} WHERE 1=CONTAINS({point},{circle});"
+print(f"SQL query for Gaia database:")
+print(f" {query}")
 
 # sending a job to Gaia database
 job = astroquery.gaia.Gaia.launch_job_async (query, dump_to_file=True, \
@@ -68,4 +68,6 @@ print (job)
 
 # getting results
 results = job.get_results ()
+
+# printing results
 print (results)
