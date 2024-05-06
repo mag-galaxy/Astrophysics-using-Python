2ce81894b84f30a4b5384cd2933cc0d450a297b9
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-06 20:13:19 +0800

    Update s11_01_07.py

--- s11_01_07.py
+++ s11_01_07.py
@@ -3,25 +3,26 @@ import numpy
 import matplotlib.figure
 import matplotlib.backends.backend_agg
 
-# construction of parser object for argparse
-descr  = 'making HR diagram'
-parser = argparse.ArgumentParser (description=descr)
-
-# adding arguments
-parser.add_argument ('-i', '--input', help='input file name')
-parser.add_argument ('-o', '--output', help='output file name')
-parser.add_argument ('-t', '--title', help='title of plot')
-parser.add_argument ('-r', '--resolution', type=float, default=150.0, \
-                     help='resolution in DPI (default: 150)')
-
-# command-line argument analysis
-args = parser.parse_args ()
+# using argparse
+descr  = 'candidate selection by proper motion of stars'
+parser = argparse.ArgumentParser(description=descr)
+parser.add_argument('-i', '--input', help='input file name')
+parser.add_argument('-o', '--output', help='output file name')
+parser.add_argument('-a1', type=float, default=0.0,help='minimum RA')
+parser.add_argument('-a2', type=float, default=0.0, help='maximum RA')
+parser.add_argument('-d1', type=float, default=0.0, help='minimum Dec')
+parser.add_argument('-d2', type=float, default=0.0, help='maximum Dec')
+parser.add_argument('-r', '--radius', type=float, default=1.0, help='radius for selection')
+args = parser.parse_args()
 
 # input parameters
 file_input     = args.input
 file_output    = args.output
-title          = args.title
-resolution_dpi = args.resolution
+pmra_min       = args.a1
+pmra_max       = args.a2
+pmdec_min      = args.d1
+pmdec_max      = args.d2
+radius         = args.radius
 
 # lists to store data
 list_id       = []
@@ -40,17 +41,13 @@ list_gr       = []
 
 # opening file
 with open (file_input, 'r') as fh:
-    # reading file
     for line in fh:
-        # if line starts with '#', then skip
         if (line[0] == '#'):
             continue
         # removing new line at the end of the line
         line = line.strip ()
-        # splitting the line
         data = line.split ()
-        # fields
-        list_id.append (int (data[0]) )
+        list_id.append (data[0])
         list_ra.append (float (data[1]) )
         list_dec.append (float (data[2]) )
         list_parallax.append (float (data[3]) )
@@ -79,45 +76,73 @@ data_br       = numpy.array (list_br)
 data_bg       = numpy.array (list_bg)
 data_gr       = numpy.array (list_gr)
 
-# clearing lists
-list_id.clear ()
-list_ra.clear ()
-list_dec.clear ()
-list_parallax.clear ()
-list_pmra.clear ()
-list_pmdec.clear ()
-list_rv.clear ()
-list_b.clear ()
-list_g.clear ()
-list_r.clear ()
-list_br.clear ()
-list_bg.clear ()
-list_gr.clear ()
+# # clearing lists
+# list_id.clear ()
+# list_ra.clear ()
+# list_dec.clear ()
+# list_parallax.clear ()
+# list_pmra.clear ()
+# list_pmdec.clear ()
+# list_rv.clear ()
+# list_b.clear ()
+# list_g.clear ()
+# list_r.clear ()
+# list_br.clear ()
+# list_bg.clear ()
+# list_gr.clear ()
 
-# calculation of g-band absolute magnitude
-data_g_abs = data_g + 5.0 * numpy.log10 (data_parallax / 1000.0) + 5.0
+# making empty lists
+list_pmra_selected  = []
+list_pmdec_selected = []
 
-# making objects "fig", "canvas", and "ax"
-fig    = matplotlib.figure.Figure ()
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
-ax     = fig.add_subplot (111)
+# finding candidate stars
+for i in range (len (data_pmra)):
+    # rejecting star if pmra is smaller than pmra_min
+    if (data_pmra[i] < pmra_min):
+        continue
+    # rejecting star if pmra is larger than pmra_max
+    if (data_pmra[i] > pmra_max):
+        continue
+    # rejecting star if pmdec is smaller than pmdec_min
+    if (data_pmdec[i] < pmdec_min):
+        continue
+    # rejecting star if pmdec is larger than pmdec_max
+    if (data_pmdec[i] > pmdec_max):
+        continue
+    # appending data to lists
+    list_pmra_selected.append (data_pmra[i])
+    list_pmdec_selected.append (data_pmdec[i])
 
-# axes
-ax.set_xlabel ('(b-r) colour index')
-ax.set_ylabel ('g absolute magnitude [mag]')
-#ax.invert_yaxis ()
-ax.grid ()
-ax.set_title (title)
-ax.set_box_aspect (aspect=1.0)
-ax.set_xlim (-1.0, 6.0)
-ax.set_ylim (18.0, -3.0)
+# calculating mean proper motion of candidate stars
+data_pmra_selected  = numpy.array (list_pmra_selected)
+data_pmdec_selected = numpy.array (list_pmdec_selected)
+mean_pmra           = numpy.mean (data_pmra_selected)
+mean_pmdec          = numpy.mean (data_pmdec_selected)
 
-# plotting vectors
-ax.plot (data_br, data_g_abs, \
-         linestyle='None', marker='o', markersize=3, color='blue', \
-         label='Stars in Gaia DR3')
-ax.legend ()
-ax.legend (bbox_to_anchor=(1.05, 0.95), loc='upper left')
+# printing results
+print (f'mean pmra and pmdec of star cluster member candidates:')
+print (f'  mean pmra  = {mean_pmra:+6.2f} [mas/yr]')
+print (f'  mean pmdec = {mean_pmdec:+6.2f} [mas/yr]')
 
-# saving file
-fig.savefig (file_output, dpi=resolution_dpi, bbox_inches="tight")
+# opening file for writing
+with open (file_output, 'w') as fh:
+    header = f'# star ID, RA, Dec, parallax, pmra, pmdec, radial velocity' \
+        + f' b mag, g mag, r mag, b-r, b-g, g-r\n'
+    fh.write (header)
+    # candidate selection
+    for i in range (len (data_id)):
+        # calculation of distance from mean pmra and pmdec
+        offset = numpy.sqrt( (data_pmra[i] - mean_pmra)**2 \
+                              + (data_pmdec[i] - mean_pmdec)**2 )
+        # if offset is larger than radius, then skip
+        if (offset > radius):
+            continue
+        # writing candidate into new file
+        record = f"{data_id[i]:19s}" \
+            + f" {data_ra[i]:10.6f} {data_dec[i]:+10.6f}" \
+            + f" {data_parallax[i]:10.6f}" \
+            + f" {data_pmra[i]:10.6f} {data_pmdec[i]:10.6f}" \
+            + f" {data_rv[i]:+10.6f}" \
+            + f" {data_b[i]:9.6f} {data_g[i]:9.6f} {data_r[i]:9.6f}" \
+            + f" {data_br[i]:9.6f} {data_bg[i]:9.6f} {data_gr[i]:9.6f}\n"
+        fh.write (record)
