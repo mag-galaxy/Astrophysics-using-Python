b3076fb0b041619d3977f073aca642110142413b
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-07 11:02:36 +0800

    Update s11_01_02.py

--- s11_01_02.py
+++ s11_01_02.py
@@ -5,14 +5,14 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # using argparse
-descr = 'distribution of distance of stars'
+descr = 'distribution of distance of stars (histogram)'
 parser = argparse.ArgumentParser(description=descr)
 parser.add_argument('-i', '--input', help='input file name (gzip)')
 parser.add_argument('-o', '--output', help='output file name (png)')
-parser.add_argument('-a', '--min', type=float, help='minimum data value')
-parser.add_argument('-b', '--max', type=float, help='maximum data value')
+parser.add_argument('-a', '--min', type=float, help='min. dist. value')
+parser.add_argument('-b', '--max', type=float, help='max. dist. value')
 parser.add_argument('-n', '--nbins', type=int, help='number of bins')
-args = parser.parse_args ()
+args = parser.parse_args()
 
 # get value from argument
 file_votable = args.input
@@ -24,14 +24,14 @@ n_bins = args.nbins
 # read input file
 table = astropy.io.votable.parse_single_table(file_votable).to_table()
 
-# data
+# data in numpy array
 data_ra = numpy.array(table['ra'])
 data_dec = numpy.array(table['dec'])
-data_parallax  = numpy.array(table['parallax'])
+data_parallax = numpy.array(table['parallax'])
 data_p_snr = numpy.array(table['parallax_over_error'])
 
-# distance
-data_distance = numpy.array([])
+# store distance data for poltting histogram
+data_distance = numpy.array([])        # empty numpy array
 for i in range(len(data_parallax)):
     # rejecting stars of negative parallax, no measurement of parallax,
     # and parallax SNR less than 10.0
@@ -45,10 +45,10 @@ for i in range(len(data_parallax)):
 fig = matplotlib.figure.Figure()
 canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
 ax = fig.add_subplot(111)
-bin_range = (x_min, x_max)      # set range of bins
+# bin_range = (x_min, x_max)      # set range of bins
 ax.set_xlabel('Distance [pc]')
 ax.set_ylabel('Number of Stars')
-ax.set_xlim(bin_range)
+ax.set_xlim(x_min, x_max)
 ax.grid()
-ax.hist(data_distance, bins=n_bins, range=bin_range, histtype='bar', align='mid')
+ax.hist(data_distance, bins=n_bins, range=(x_min, x_max), histtype='bar', align='mid')
 fig.savefig (file_fig, dpi=150)
