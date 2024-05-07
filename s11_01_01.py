4fcec1b495e25dd4bbc3bdd0629a6119e347251b
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-07 10:56:20 +0800

    Update s11_01_01.py

--- s11_01_01.py
+++ s11_01_01.py
@@ -12,23 +12,22 @@ astroquery.gaia.Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 astroquery.gaia.Gaia.ROW_LIMIT = -1
 
 # using argparse
-descr = 'Gaia DR3 Catalogue downloading'
+descr = 'download Gaia DR3 Catalogue'
 parser = argparse.ArgumentParser(description=descr)
 parser.add_argument('-t', '--target', help='target name')
-parser.add_argument('-o', '--output', help='output file name')
+parser.add_argument('-o', '--output', help='output file name (gzip)')
 parser.add_argument('-r', '--radius', type=float, help='radius of search in arcmin')
-args = parser.parse_args ()
+args = parser.parse_args()
 
 # get value from argument
 target = args.target
-file_output = args.output    # output in a gzip file
+file_output = args.output
 radius_arcmin = args.radius
 radius_deg = radius_arcmin / 60.0
 
 # units
 u_deg = astropy.units.deg
 u_ha = astropy.units.hourangle
-u_arcmin = astropy.units.arcmin
 
 # find target using Simbad and get its coordinate using Skycoord
 result_simbad = astroquery.simbad.Simbad.query_object(target)
@@ -36,21 +35,20 @@ tar_ra = result_simbad['RA'][0]
 tar_dec = result_simbad['DEC'][0]
 coord = astropy.coordinates.SkyCoord(tar_ra, tar_dec, frame='icrs', unit=(u_ha, u_deg) )
 
-# coordinate in decimal degree
+# coordinate in degree
 ra_deg = coord.ra.deg
 dec_deg = coord.dec.deg
 
-# command for database query
+# command for database SQL query
 table = f"gaiadr3.gaia_source"
 field = f"*"
 point = f"POINT({ra_deg:8.4f},{dec_deg:8.4f})"
 circle = f"CIRCLE(ra,dec,{radius_deg})"
 query = f"SELECT {field} from {table} WHERE 1=CONTAINS({point},{circle});"
-print(f"SQL query for Gaia database:\n {query}")
+print(f"SQL query for Gaia database:\n\t{query}")
 
 # sending a job to Gaia database
 job = astroquery.gaia.Gaia.launch_job_async(query, dump_to_file=True, \
                   output_format="votable_gzip", output_file=file_output)
-print (job)
 results = job.get_results ()
 print (results)
