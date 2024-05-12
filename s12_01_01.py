47f158b1d081c9f3d2969722d5910fc3b9e0a46c
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-12 16:50:45 +0800

    Update s12_01_01.py

--- s12_01_01.py
+++ s12_01_01.py
@@ -1,1 +1,25 @@
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+list_url = [
+    'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/755/20/kplr010666592-2013017113907_slc.fits', \
+    'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/755/20/kplr010666592-2013065031647_slc.fits', \
+    'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/755/20/kplr010666592-2013098041711_slc.fits']
+
 # download Kepler data
+for url_data in list_url:
+    file_output_name = url_data.split('/')[-1]
+    print (f'Now, fetching {url_data}...')
+    with urllib.request.urlopen(url_data) as fh_read:
+        data_byte = fh_read.read ()
+
+    print (f'Finished fetching {url_data}!')
+    print (f'Now, writing data into file "{file_output_name}"...')
+
+    # write data
+    with open (file_output_name, 'wb') as fh_write:
+        fh_write.write (data_byte)
+
+    print (f'Finished writing data into file "{file_output_name}"!')
