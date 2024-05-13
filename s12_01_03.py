6c824b43c70571cf62c85329c8fee98c77f85eec
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-13 09:52:43 +0800

    Update s12_01_03.py

--- s12_01_03.py
+++ s12_01_03.py
@@ -4,10 +4,10 @@ import matplotlib.figure
 import matplotlib.backends.backend_agg
 
 # using argparse
-descr  = 'plotting time-series data taken by Kepler Space Telescope'
+descr = 'plotting time-series data taken by Kepler Space Telescope'
 parser = argparse.ArgumentParser (description=descr)
 parser.add_argument('-i', type=str, default="in.data", help='input file name (data)')
-parser.add_argument('-o', type=str, default="out.eps", help='output file name (png)')
+parser.add_argument('-o', type=str, default="out.png", help='output file name (png)')
 args = parser.parse_args()
 
 # get value from argument
