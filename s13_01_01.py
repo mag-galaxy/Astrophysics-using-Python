053bdd3c2cdc15a7058f89f0513976c713314aa0
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-14 20:33:01 +0800

    Create s13_01_01.py

---
+++ s13_01_01.py
@@ -0,0 +1,7 @@
+# follow practice A and download fits file from HST and JWSt
+# then do source extraction, star-to-star matching, image alignment
+# 06_00 ~ 06_06, 07_00 ~ 07_03
+
+# source extraction, for both HST and JWST fits
+# 02_02 fits file to cat file
+# 02_04 create png file
