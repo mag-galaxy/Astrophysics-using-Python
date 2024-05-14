be687f50c3505b6aa1c762043617ea8b716a171b
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-14 08:55:11 +0800

    Update s12_01_05.py

--- s12_01_05.py
+++ s12_01_05.py
@@ -29,11 +29,11 @@ with open (input_data, 'r') as fh_read:
                 continue
             line = line.strip()
             all_data = line.split()
-            (datetime_str, mjd_str, flux_str, err_str) = line.split ()
+            datetime_str = all_data[0]
             if (all_data[2] == 'nan'):
                 continue
             mjd = float(all_data[1])
             flux = float(all_data[2]) / (-a * mjd + b)
             err = float(all_data[3]) / float(all_data[2])
             record = f"{datetime_str} {mjd:.10f} {flux:.10f} {err:.10f}\n"
-            fh_write.write (record)
+            fh_write.write(record)
