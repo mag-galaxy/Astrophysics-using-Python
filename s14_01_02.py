9c91af621c002d6f0308f76156417954951ea8c5
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-26 21:46:19 +0800

    Update s14_01_02.py

--- s14_01_02.py
+++ s14_01_02.py
@@ -4,14 +4,13 @@ import rebound
 file_sim = 'sirius.bin'    # input file name (bin)
 file_out = 'sirius.data'   # output file name (data)
 
-sim = rebound.Simulation(file_sim)  # read binary file
+sim = rebound.Simulation(file_sim)  # read binary system file
 sim.move_to_com()  # move to centre of momentum frame
 ps = sim.particles
 
-# parameters for simulation
-t_interval = 1.0
+# for simulation
+t_interval = 1.0    # time interval
 n_output = 1000  # number of output data
-
 sim.integrator = 'ias15'
 sim.dt = 0.01
 
@@ -22,16 +21,12 @@ with open (file_out, 'w') as fh:
 
     # orbital integration
     for i in range(n_output):
-        # time
         t = t_interval * i
         t_yr = t_interval * i / (2.0 * numpy.pi)
-        # orbital integration for a time step
         sim.integrate(t)
-        # position of Sirius A at time t
         sirA_x = ps[0].x
         sirA_y = ps[0].y
         sirA_z = ps[0].z
-        # position of Sirius B at time t
         sirB_x = ps[1].x
         sirB_y = ps[1].y
         sirB_z = ps[1].z
