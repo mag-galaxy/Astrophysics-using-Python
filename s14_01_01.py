09219e61f7ffea36517504dd969f9f2aa12d15d8
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-26 20:54:16 +0800

    Update s14_01_01.py

--- s14_01_01.py
+++ s14_01_01.py
@@ -1,11 +1,17 @@
 import rebound
+import numpy
 
 file_sim = 'sirius.bin'
+
+# calculate semimajor axis
+a_arcsec = 7.4957
+distance = 2.6  # pc
+a_au = a_arcsec * distance
 
 # make simulation object using rebound
 sim = rebound.Simulation()
 sim.add(m=2.063)  # Sirius A
-sim.add(m=1.018)  # Sirius B
+sim.add(m=1.018, a = a_au, e = 0.59142)  # Sirius B
 
 print (sim)
 sim.save_to_file(file_sim)
