1054923cb009337190294b4c9892f8fdb71feba9
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-26 19:51:07 +0800

    Update s14_01_04.py

--- s14_01_04.py
+++ s14_01_04.py
@@ -5,10 +5,10 @@ ffmpeg = 'ffmpeg'
 
 # ffmpeg options
 options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
-    + f' -i binary/binary_%08d.png' \
+    + f' -i sirius/sirius_%08d.png' \
     + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
 
-file_output = 'sirius_binary.mp4'  # output file name (mp4)
+file_output = 'sirius.mp4'  # output file name (mp4)
 
 # command
 command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
