fe8dfcc063214d587c3e1aa3b8a0c018c9a4c264
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-26 12:29:22 +0800

    Create s14_01_01.py

---
+++ s14_01_01.py
@@ -0,0 +1,11 @@
+import rebound
+
+file_sim = 'sirius_binary.bin'
+
+# make simulation object using rebound
+sim = rebound.Simulation()
+sim.add(m=2.063)  # Sirius A
+sim.add(m=1.018)  # Sirius B
+
+print (sim)
+sim.save_to_file(file_sim)
