1952c048776a4d28995344415991cd524cc0806b
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-05-29 15:13:47 +0800

    Update s15_01_03.py

--- s15_01_03.py
+++ s15_01_03.py
@@ -6,13 +6,12 @@ import sklearn.cluster
 # use argparse
 descr = 'finding star cluster members using parallax, rv, pmra, and pmdec'
 parser = argparse.ArgumentParser(description=descr)
-parser.add_argument('-i', '--input', help='input file name')
-parser.add_argument('-o', '--output', help='output file name')
+parser.add_argument('-i', '--input', help='input file name (data)')
+parser.add_argument('-o', '--output', help='output file name (data)')
 parser.add_argument('-d', '--norm-factor-distance', type=float, default=30, help='normalisation factor for distance in pc')
 parser.add_argument('-r', '--norm-factor-rv', type=float, default=2, help='normalisation factor for rv in km/s')
 parser.add_argument('-e', '--eps', type=float, default=1.0, help='parameter eps for DBSCAN analysis')
 parser.add_argument('-n', '--min-samples', type=int, default=30, help='parameter min-samples for DBSCAN analysis')
-parser.add_argument('-v', '--verbose', action='store_true', default=False, help='verbose mode')
 args = parser.parse_args()
 
 # get value from argument
@@ -22,7 +21,6 @@ normfactor_dist = args.norm_factor_distance
 normfactor_rv = args.norm_factor_rv
 eps = args.eps
 min_samples = args.min_samples
-verbose = args.verbose
 
 # empty lists for storing data
 list_id = []
@@ -37,8 +35,7 @@ list_rv = []
 list_rv_norm = []
 list_stars = []
 
-if (verbose):
-    print (f'Now, reading Gaia DR3 data file...')
+print(f'Now, reading Gaia DR3 data file...')
 
 # read input file for data
 with open (file_input, 'r') as fh:
@@ -69,11 +66,8 @@ with open (file_input, 'r') as fh:
         list_rv.append(rv)
         list_rv_norm.append(rv_norm)
        
-if (verbose):
-    print (f'Finished reading Gaia DR3 data file!')
-
-if (verbose):
-    print (f'Now, constructing Numpy array...')
+print(f'Finished reading Gaia DR3 data file!')
+print(f'Now, constructing Numpy array...')
 
 # change into numpy arrays
 array_id = numpy.array(list_id)
@@ -88,31 +82,23 @@ array_rv = numpy.array(list_rv)
 array_rv_norm = numpy.array(list_rv_norm)
 
 # constructing a Numpy array for DBSCAN analysis
-stars = numpy.stack ([array_dist_norm, array_pmra, array_pmdec, array_rv_norm]).transpose()
-    
-if (verbose):
-    print (f'Finished constructing Numpy array!')
-    
-if (verbose):
-    print (f'Now, doing DBSCAN analysis...')
+stars = numpy.stack([array_dist_norm, array_pmra, array_pmdec, array_rv_norm]).transpose()
+
+print(f'Finished constructing Numpy array!')
+print(f'Now, doing DBSCAN analysis...')
 
 # clustering analysis using DBSCAN
 result_dbscan = sklearn.cluster.DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1).fit(stars)
 labels = result_dbscan.labels_
 
-if (verbose):
-    print (f'Finished doing DBSCAN analysis!')
-
-if (verbose):
-    print (f'Now, writing results into output file...')
+print(f'Finished doing DBSCAN analysis!')
+print(f'Now, writing results into output file...')
 
 # write clustring result into output file
 with open (file_output, 'w') as fh:
     header = f'#\n' \
         + f'# Results of DBSCAN analysis\n' \
-        + f'#\n' \
         + f'#  format of this data file\n' \
-        + f'#\n' \
         + f'#   column 01 : source ID\n' \
         + f'#   column 02 : RA in deg\n' \
         + f'#   column 03 : Dec in deg\n' \
@@ -123,23 +109,13 @@ with open (file_output, 'w') as fh:
         + f'#   column 08 : proper motion in Dec in mas/yr\n' \
         + f'#   column 09 : radial velocity in km/s\n' \
         + f'#   column 10 : normalised radial velocity\n' \
-        + f'#   column 11 : labels (0=1st cluster, 1=2nd cluster, ... , -1=field stars)\n' \
-        + f'#\n'
+        + f'#   column 11 : labels (0=1st cluster, 1=2nd cluster, ... , -1=field stars)\n'
     fh.write(header)
 
     for i in range(len(array_id)):
-        record = f"{array_id[i]:19d}" \
-            + f" {array_ra[i]:12.8f}" \
-            + f" {array_dec[i]:12.8f}" \
-            + f" {array_parallax[i]:8.3f}" \
-            + f" {array_dist[i]:8.3f}" \
-            + f" {array_dist_norm[i]:8.3f}" \
-            + f" {array_pmra[i]:8.3f}" \
-            + f" {array_pmdec[i]:8.3f}" \
-            + f" {array_rv[i]:12.6f}" \
-            + f" {array_rv_norm[i]:12.6f}" \
-            + f" {labels[i]}\n"
+        record = f"{array_id[i]:19d} {array_ra[i]:12.8f} {array_dec[i]:12.8f}" \
+            + f" {array_parallax[i]:8.3f} {array_dist[i]:8.3f} {array_dist_norm[i]:8.3f}" \
+            + f" {array_pmra[i]:8.3f} {array_pmdec[i]:8.3f} {array_rv[i]:12.6f}" \
+            + f" {array_rv_norm[i]:12.6f} {labels[i]}\n"
         fh.write(record)
-
-if (verbose):
-    print (f'Finished writing results into output file!')
+    print(f'Finished writing results into output file!')
