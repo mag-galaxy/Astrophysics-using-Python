6e1a0a8c01ea8a01dca011797ec47b9e415276bc
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-06-04 20:20:55 +0800

    Update s16_exercise_a.py

--- s16_exercise_a.py
+++ s16_exercise_a.py
@@ -10,12 +10,14 @@ parser.add_argument('-l1', '--long', help='longitude of observing site (dms)')
 parser.add_argument('-l2', '--lat', help='latitude of observing site (dms)')
 parser.add_argument('-e', '--height', help='height of observing site (m)')
 parser.add_argument('-d', '--date', help='observing date (yyyy-mm-dd)')
+parser.add_argument('-z', '--timezone', help='timezone of observing site ')
 args = parser.parse_args()
 
 # get value from argument
 long = args.long
 lat = args.lat
 date = args.date
+timez = args.timezone
 height = float(args.height)
 
 # units
@@ -27,7 +29,7 @@ obs_time = astropy.time.Time(time_str, format='iso', scale='utc')
 
 # observer
 height = height * m
-observer = astroplan.Observer(longitude=long, latitude=lat, elevation=height, name='observer')
+observer = astroplan.Observer(longitude=long, latitude=lat, elevation=height, name='observer', timezone=timez)
 
 # sun rise time
 sun_rise = observer.sun_rise_time(obs_time, which='next')
